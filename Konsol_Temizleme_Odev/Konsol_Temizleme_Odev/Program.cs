a864df91f39764e22457d5da0b8076aabe260d02
ErMamurer <erenmamurer@gmail.com>
2025-10-07 00:58:05 +0300

    Son ve kayıp olan Konsol Temizleme projesi eklendi

---
+++ Konsol_Temizleme_Odev/Konsol_Temizleme_Odev/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Konsol_Temizleme_Odev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
